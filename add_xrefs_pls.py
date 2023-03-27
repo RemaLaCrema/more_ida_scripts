7f7c63c3a2c3a16b4036e016831ffae64a221ab8
cnccdan <cnccdan@gmail.com>
2023-03-27 16:27:22 +0300

    Create add_xrefs_pls.py

---
+++ add_xrefs_pls.py
@@ -0,0 +1,28 @@
+import idautils
+import idaapi
+import idc
+
+def add_xrefs():
+    for func in idautils.Functions():
+        funcName = idc.get_func_name(func)
+        xRefCount = len(list(idautils.XrefsTo(func)))
+        idc.set_name(func, "_" + str(xRefCount) + "_" + funcName)
+        
+class add_xrefs_t(idaapi.plugin_t):
+    comment = "Adding Xrefs count to func names"
+    help = ""
+    wanted_name = "add_xrefs"
+    wanted_hotkey = ""
+    flags = idaapi.PLUGIN_UNL
+    
+    def init(self):
+        return idaapi.PLUGIN_OK        
+
+    def run(self, arg):
+        add_xrefs()
+
+    def term(self):
+        return
+
+def PLUGIN_ENTRY():
+    return add_xrefs_t()
